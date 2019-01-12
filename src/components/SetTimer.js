b2669b3825072178a910ad70d36f4c7f6a7a6db1
hColleen <hcolleen@gmail.com>
2019-01-12 03:00:50 -0700

    timer accepts input, start working on timer state/start button funciton

--- src/components/SetTimer.js
+++ src/components/SetTimer.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class SetTimer extends Component{
+
+    constructor(){
+        super();
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(ev){
+        const newBaseTime = this.props.baseTime
+        if (ev.target.id === 'hours') newBaseTime.subtract(newBaseTime.get('hour'), 'hours').add(parseInt(ev.target.value, 10), 'hours');
+        if (ev.target.id === 'minutes') newBaseTime.subtract(newBaseTime.get('minutes'), 'minutes').add(parseInt(ev.target.value, 10), 'minutes');
+        if (ev.target.id === 'seconds') newBaseTime.subtract(newBaseTime.get('seconds'), 'seconds').add(parseInt(ev.target.value, 10), 'seconds');
+
+        this.props.setBaseTime(newBaseTime)
+    }
+
+
+
     render() {
         return (
           <div className="row">
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import TimerDisplay from './TimerDisplay';
 import SetTimer from './SetTimer';
-import moment from 'moment'
+import moment from 'moment';
+import * as timerStates from '../timerStates'
 
 export default class Timer extends Component {
 
@@ -10,14 +11,26 @@ export default class Timer extends Component {
         this.state = {
             currentTime: moment.duration(25, 'minutes'),
             baseTime: moment.duration(25, 'minutes'),
+            timerState: timerStates.NOT_SET,
         }
+         this.setBaseTime = this.setBaseTime.bind(this);
+    }
+
+    setBaseTime(newBaseTime){
+        this.setState({
+            baseTime: newBaseTime,
+            currentTime: newBaseTime,
+        })
     }
 
     render(){
         return(
             <div className = "timer">
             <TimerDisplay currentTime ={this.state.currentTime} />
-            <SetTimer />
+            <SetTimer 
+            baseTime = {this.state.baseTime}
+            setBaseTime = {this.setBaseTime}
+            />
             </div>
         )
     }
