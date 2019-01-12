79871a76fa7f748bcaa32397c2884c264b9dc972
hColleen <hcolleen@gmail.com>
2019-01-12 03:19:44 -0700

    set reduceTime funciton, create buttons

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,6 +3,7 @@ import TimerDisplay from './TimerDisplay';
 import SetTimer from './SetTimer';
 import moment from 'moment';
 import * as timerStates from '../timerStates'
+import TimerButton from './TimerButton';
 
 export default class Timer extends Component {
 
@@ -12,8 +13,11 @@ export default class Timer extends Component {
             currentTime: moment.duration(25, 'minutes'),
             baseTime: moment.duration(25, 'minutes'),
             timerState: timerStates.NOT_SET,
+            timer: null,
         }
          this.setBaseTime = this.setBaseTime.bind(this);
+         this.startTimer = this.startTimer.bind(this);
+         this.reduceTimer = this.reduceTimer.bind(this);
     }
 
     setBaseTime(newBaseTime){
@@ -23,14 +27,37 @@ export default class Timer extends Component {
         })
     }
 
+    startTimer(){
+        this.setState({
+            timerState: timerStates.RUNNING,
+            timer: setInterval(this.reduceTimer, 1000),
+        })
+
+    }
+
+    reduceTimer(){
+        const newTime = moment.duration(this.state.currentTime);
+        newTime.subtract(1, 'second')
+        this.setState({
+            currentTime: newTime,
+        })
+    }
+
     render(){
         return(
             <div className = "timer">
             <TimerDisplay currentTime ={this.state.currentTime} />
-            <SetTimer 
-            baseTime = {this.state.baseTime}
-            setBaseTime = {this.setBaseTime}
-            />
+            <TimerButton startTimer = {this.startTimer} />
+            {
+                (this.state.timerState !== timerStates.RUNNING)
+                &&
+                (
+                    <SetTimer 
+                    baseTime = {this.state.baseTime}
+                    setBaseTime = {this.setBaseTime}
+                    />
+                )
+            }
             </div>
         )
     }
