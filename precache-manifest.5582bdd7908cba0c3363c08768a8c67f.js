16382ca2ca697843c5e01c754985782bc2289a22
hColleen <hcolleen@gmail.com>
2019-01-13 05:03:15 -0700

    Updates

---
+++ precache-manifest.5582bdd7908cba0c3363c08768a8c67f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d79190841269576269db24d199b92a3b",
+    "url": "/pomodoro-clock-react/static/media/night_sky.d7919084.jpg"
+  },
+  {
+    "revision": "343526ac4728393ee573",
+    "url": "/pomodoro-clock-react/static/js/runtime~main.343526ac.js"
+  },
+  {
+    "revision": "b139243385c3bae6c359",
+    "url": "/pomodoro-clock-react/static/js/main.b1392433.chunk.js"
+  },
+  {
+    "revision": "37731ac9fc772241fde9",
+    "url": "/pomodoro-clock-react/static/js/1.37731ac9.chunk.js"
+  },
+  {
+    "revision": "b139243385c3bae6c359",
+    "url": "/pomodoro-clock-react/static/css/main.903b7a5d.chunk.css"
+  },
+  {
+    "revision": "37731ac9fc772241fde9",
+    "url": "/pomodoro-clock-react/static/css/1.49c4bf78.chunk.css"
+  },
+  {
+    "revision": "e1529177a85c467343f7a08c69197453",
+    "url": "/pomodoro-clock-react/index.html"
+  }
+];
